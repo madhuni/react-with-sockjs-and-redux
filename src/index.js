c9246e39f76982275d8f4d6d17aa1f588acba4b1
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-03-30 19:22:43 +0530

    Setup the default settings for Axios

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { combineReducers } from "redux";
 
+import axios from "axios";
+
 import App from './app.jsx';
 import initialDataReducer from './store/reducers/initial-data';
 import socketDataReducer from './store/reducers/socket-data';
 import './index.css';
+
+/* Setting up the default settings for Axios */
+axios.defaults.baseURL = 'http://0.0.0.0:5000/api/';
+// axios.defaults.baseURL = 'http://192.168.1.150:5000/api/';
+axios.defaults.headers.common['X-Api-Key'] = '';
 
 /* Creating a 'rootReducer' with the mulitple reducers */
 const rootReducer = combineReducers({

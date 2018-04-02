06bd9938fece8ccbc419536424fde68e6f372f42
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-04-02 11:37:57 +0530

    Removed unused buttons from nav-bar

--- src/container/home/components/nav-bar/nav-bar.jsx
+++ src/container/home/components/nav-bar/nav-bar.jsx
@@ -3,23 +3,38 @@ import { NavLink } from 'react-router-dom';
 
 import './nav-bar.css'
 
-import forwardIcon from '../../../../assets/images/forward-icon.png'
+import forwardIcon from '../../../../assets/images/forward-icon.png';
 
 import Button from '../../../../components/button/button';
 
 const navBar = (props) => {
+  let reconnectClass;
+  let btnName;
+
+  if (props.flags !== null) {
+    if (props.flags.printing || props.flags.paused || props.flags.operational) {
+      reconnectClass = ['btn--control btn--green'];
+      btnName = 'Connected';
+    } else {
+      reconnectClass = ['btn--control btn--danger'];
+      btnName = 'Re-Connect'
+    }
+  } else {
+    reconnectClass = ['btn--disable'];
+    btnName = 'Connected'
+  }
+
   return (
     <nav className="nav-bar flex-row">
       <h1 className="current-state">Status: {props.currentState}</h1>
       <div className="controls">
-        <Button classValue={"btn--control btn--green"} name={"Connect"} clicked={props.reconnectPrinter}/>
-        <Button classValue={"btn--control"} name={"Tool-0"} clicked={props.tool0}/>
-        <Button classValue={"btn--control"} name={"Tool-1"} clicked={props.tool1}/>
+        <Button classValue={reconnectClass.join(' ')} name={btnName} clicked={props.reconnectPrinter}/>
       </div>
-      <div className="route-btn">
+      <div className="route-btn flex-row">
         <NavLink to='/control'>
-          Controls {"-->"}
+          Controls 
         </NavLink>
+        <img src={forwardIcon} alt="Forward" width="36" height="36" />
       </div>
     </nav>
   );

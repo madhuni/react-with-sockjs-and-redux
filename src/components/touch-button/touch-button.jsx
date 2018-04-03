2e0b59ba607f9025223a05a81cf9789cf1f387c1
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-04-03 19:03:44 +0530

    Add new TouchButton global component

---
+++ src/components/touch-button/touch-button.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const touchButton = (props) => {
+  // onTouchStart = { props.touched }
+  return (
+    <button className={props.classValue}
+      onTouchStart={props.touched}
+      onTouchEnd={props.stop}
+      // onMouseDown={props.touched}
+      // onMouseUp={props.stop}
+    >
+      {props.name}
+      {props.children}
+    </button>
+  );
+};
+
+export default touchButton;

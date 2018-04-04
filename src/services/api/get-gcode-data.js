abcedc5f1e4c1bfc985fee6ee440ede9ae01a6f4
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-04-04 21:09:08 +0530

    Add api service to fetch the Gcode data
    Data contains the 'layer_count' and 'total_time'

---
+++ src/services/api/get-gcode-data.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const getGcodeData = (callback, filename) => {
+  /**
+     * Fetching the Initial Data once the App component is mounted
+     * 
+     * Once the initial data is fetched, this value will be set to
+     * the State in our Redux Store.
+    */
+
+  const fileNames = [
+    '3hr_print.gcode',
+    '2hr_print.gcode',
+    '3min_print.gcode',
+    'ERAY_electron_testing.gcode',
+    'ERAY_20mmTestCuberepaired.gcode',
+    'ERAY_darthvader_lowpoly_body.gcode',
+    'RAY_InceptionTopFinal.gcode',
+    'Chess_Board_dual.gcode'
+  ];
+
+  axios.get('/gcode_data/local/' + filename)
+    .then(res => {
+      // console.log(res.data);
+      callback(res);
+    })
+    .catch(err => {
+      if (err.response) {
+        console.log(err.response);
+      } else {
+        console.log(err);
+      }
+    });
+};
+
+export default getGcodeData;
